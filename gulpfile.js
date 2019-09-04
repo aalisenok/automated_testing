d623925f97fa31290f0b3eed4002a068826d5e8b
Andrey Alisenok <aalisenak@gmail.com>
2019-09-04 19:41:47 +0300

    fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,66 @@
-const gulp = require('gulp'),
-    browserSync = require('browser-sync').create(),
+const gulp = require('gulp');
+    mjml = require('gulp-mjml'),
+    // browserSync = require('browser-sync').create(),
     imagemin = require('gulp-imagemin'),
-    // imageminJpegRecompress = require('imagemin-jpeg-recompress'),
-    imageminMozjpeg = require('imagemin-mozjpeg'),
-    imageminGiflossy = require('imagemin-giflossy'),
     imageminPngquant = require('imagemin-pngquant'),
-    del = require("del"),
-    newer = require('gulp-newer');
+    imageminZopfli = require('imagemin-zopfli'),
+    imageminMozjpeg = require('imagemin-mozjpeg'),
+    newer = require('gulp-newer'),
+
+        imageminGiflossy = require('imagemin-giflossy');
+// del = require("del"),
+
+gulp.task('default', function () {
+    return gulp.src('index.mjml')
+        .pipe(mjml())
+        .pipe(gulp.dest('build'))
+});
+
 
 // IMAGES
 gulp.task('images', () => {
     return gulp.src('./src/img/**')
         .pipe(newer('./build/img/'))
         .pipe(imagemin([
-                imagemin.gifsicle({interlaced: true}),
-                imageminGiflossy({
-                    optimizationLevel: 3,
-                    optimize: 3,
-                    lossy: 2
-                }),
-                imagemin.jpegtran({progressive: true}),
-                // imageminJpegRecompress({
-                //     loops: 5,
-                //     min: 70,
-                //     max: 80,
-                //     quality: 'high'
-                // }),
-                imageminMozjpeg({
-                    progressive: true,
-                    quality: 90
-                }),
-                imagemin.optipng({optimizationLevel: 3}),
-                imageminPngquant({
-                    quality: [0.6, 0.8],
-                    speed: 5
-                })],
-            {
-                verbose: true
-            }
-        ))
+            imageminPngquant({
+                speed: 5,
+                quality: [0.6, 0.8]
+            }),
+            imageminZopfli({
+                more: true
+            }),
+            imageminMozjpeg({
+                progressive: true,
+                quality: 70
+            })
+            ,
+            imageminGiflossy({
+                optimizationLevel: 3,
+                optimize: 3,
+                lossy: 2
+            }),
+        ]))
         .pipe(gulp.dest('./build/img/'))
 });
 
 // WATCH
-gulp.task('watch', () => {
-    gulp.watch('./src/scss/**/*.scss', gulp.series('inky'));
-    gulp.watch('./src/templates/**/*.html', gulp.series('inky'));
-});
+// gulp.task('watch', () => {
+//     gulp.watch('./src/scss/**/*.scss', gulp.series('inky'));
+//     gulp.watch('./src/templates/**/*.html', gulp.series('inky'));
+// });
 
 //CLEAN
-gulp.task('del', () => {
-    return del(['./build']);
-});
+// gulp.task('del', () => {
+//     return del(['./build']);
+// });
 
 // SERVE
-gulp.task('serve', () => {
-    browserSync.init({
-        server: './build'
-    });
-});
+// gulp.task('serve', () => {
+//     browserSync.init({
+//         server: './build'
+//     });
+// });
 
-gulp.task('default', gulp.series('del','images',
-    gulp.parallel('watch', 'serve')
-));
+// gulp.task('default', gulp.series('del','images',
+//     gulp.parallel('watch', 'serve')
+// ));
